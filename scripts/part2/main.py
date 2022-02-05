8222d8923b5cbf83355111036c973b5c9a951c06
siljabl <siljabl@protonmail.com>
2022-02-05 15:23:19 +0100

    part2/main.py working

--- scripts/part2/main.py
+++ scripts/part2/main.py
@@ -2,32 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-from model_equations import initialise_system, evolve_system
+from model_equations import initialise_system, evolve_system, savedata
 
 
 ###########################
 ## Simulation parameters ##
 ###########################
 # np.random.seed(2)
-saving = True				# saving data
-
-model = 1                   # models: 1 (triggered), 2 (spontaneous, one lag time), 3 (spontaneous, two lag times)
+saving = True						# saving data
+model = 1							# models: 1 (triggered), 2 (spontaneous, one lag time), 3 (spontaneous, two lag times)
 mutation = True
 extinction = False
-mutation_seed = 'min'		# seeds: min , max, optimal parameters
-tot_cycles = 100 #10_000 		# total number of cycles / lenght of evolution
-
-data_path = "model_" + str(model) + "/case_" + case + "/data/seed_" + mut_seed
+mutation_seed = 'min'				# seeds: 'min' , 'max', 'optimal' parameters
+tot_cycles = 10 #10_000				# total number of cycles / lenght of evolution
 
 
 
 ###########################
 ## Antibiotic parameters ##
 ###########################
-T0 = 6.0				# application time
-Tab = 10.0				# duration
-T = T0 + Tab				# removal time
-p_arr = np.linspace(0.1, 0.9, 5) 	# application probability
+T0 = 0								# application time
+Tab = 6 							# duration
+T = T0 + Tab 						# removal time
+p_arr = np.linspace(0.1, 0.9, 2)	# application probability
 
 
 
@@ -37,6 +34,7 @@ p_arr = np.linspace(0.1, 0.9, 5) 	# application probability
 # looping though probabilities
 for p in p_arr:
     toc = time.time()
+
     # ramdom array for determining if applying ab
     r_arr = np.random.rand(tot_cycles)
     sim_args = [model, mutation, extinction, mutation_seed, r_arr]
@@ -48,9 +46,5 @@ for p in p_arr:
     tic = time.time()
     print(f'{tic-toc:2.4f} s')
 
-    savedata(ab_args, sim_args, saving)
-
-
-
-
+    savedata(ab_args, sim_args, data, saving)
 
