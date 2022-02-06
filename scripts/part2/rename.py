8575e665453cd7225c7ef40fd2a288c58ab7d768
siljabl <siljabl@protonmail.com>
2022-02-06 13:10:39 +0100

    adding model 2 mutation data

--- scripts/part2/rename.py
+++ scripts/part2/rename.py
@@ -7,38 +7,47 @@ import os
 # Function to rename multiple files
 def main():
    
-    folder = "../../data/model3/"
-    for filename in os.listdir(folder+"rename/"):
+    folder = "../../data/model2/rename/"
+    for filename in os.listdir(folder):
         src = f"{filename}"  # foldername/filename, if .py file is outside folder
         print(src)
 
         dst = src.split('0.')
         dst = dst[0] + dst[1]
-        # dst = src.split('-T0')
-        # dst = dst[0] + "0-T0" + dst[1]
 
-        # if len(src.split('l-')) == 2:
-        #     _, dst = src.split('l-')
-        #     dst = f"{dst}-optimal"
+        dst = dst.split('-T0')
+        dst = dst[0] + "0-T0" + dst[1]
 
-        # if len(src.split('t-')) == 2:
-        #     _, dst = src.split('t-')
-        #     dst = f"{dst}-min"
+        if len(dst.split('.0')) == 3:
+            print(dst)
+            dst = dst.split('.0')
+            print(dst)
+            dst = dst[0] + dst[1] + dst[2]
+            print(dst)
+
+        if len(dst.split('l-')) == 2:
+            _, dst = dst.split('l-')
+            dst = f"{dst}-optimal"
+
+        if len(dst.split('t-')) == 2:
+            _, dst = dst.split('t-')
+            dst = f"{dst}-min"
         
-        # if len(src.split('x-')) == 2:
-        #     _, dst = src.split('x-')
-        #     dst = f"{dst}-max"
+        if len(dst.split('x-')) == 2:
+            _, dst = dst.split('x-')
+            dst = f"{dst}-max"
 
         dst = f"{folder}/{dst}"
 
          
         # rename() function will
         # rename all the files
-        os.rename(folder+"rename/"+src, dst)
+        os.rename(folder+src, dst)
 
-    for filename in os.listdir(folder+"rename/"):
-        src = f"{filename}"  # foldername/filename, if .py file is outside folder
-        os.remove(folder+"rename/"+src)
+
+    # for filename in os.listdir(folder):
+    #     src = f"{filename}"  # foldername/filename, if .py file is outside folder
+    #     os.remove(folder+src)
  
 # Driver Code
 if __name__ == '__main__':
